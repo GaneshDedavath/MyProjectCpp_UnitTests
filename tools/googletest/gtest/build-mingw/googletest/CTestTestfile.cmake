6033d633a51b523ac1ec11657202566b01185566
GaneshDedavath <ganeshdedavath.official@gmail.com>
2025-05-27 21:30:41 +0530

    Removed the build-mingw dir

--- tools/googletest/gtest/build-mingw/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Workspace/MyProject_CppExamples/tools/googletest/gtest/googletest
-# Build directory: D:/Workspace/MyProject_CppExamples/tools/googletest/gtest/build-mingw/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
